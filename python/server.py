baedff580e43c809ec7201d549b8cf4f86e4eef2
Christopher Burns <cdburns22@gmail.com>
2022-03-02 10:25:44 -0500

    current status 3/2

--- python/server.py
+++ python/server.py
@@ -13,29 +13,29 @@ app = Flask(__name__)
 
 
 # Fill in your Plaid API keys - https://dashboard.plaid.com/account/keys
-PLAID_CLIENT_ID = os.getenv('PLAID_CLIENT_ID')
-PLAID_SECRET = os.getenv('PLAID_SECRET')
-PLAID_PUBLIC_KEY = os.getenv('PLAID_PUBLIC_KEY')
+PLAID_CLIENT_ID = '5525d1fa89406c556060880e'
+PLAID_SECRET = '5ea076384962d07590055cc88c73f9'
+PLAID_PUBLIC_KEY = '38a6cf789d06982c9962639fdd9b7c'
 # Use 'sandbox' to test with Plaid's Sandbox environment (username: user_good,
 # password: pass_good)
 # Use `development` to test with live users and credentials and `production`
 # to go live
-PLAID_ENV = os.getenv('PLAID_ENV', 'sandbox')
+PLAID_ENV = 'development'
 # PLAID_PRODUCTS is a comma-separated list of products to use when initializing
 # Link. Note that this list must contain 'assets' in order for the app to be
 # able to create and retrieve asset reports.
-PLAID_PRODUCTS = os.getenv('PLAID_PRODUCTS', 'transactions')
+PLAID_PRODUCTS = 'transactions'
 
 # PLAID_COUNTRY_CODES is a comma-separated list of countries for which users
 # will be able to select institutions from.
-PLAID_COUNTRY_CODES = os.getenv('PLAID_COUNTRY_CODES', 'US,CA,GB,FR,ES')
+PLAID_COUNTRY_CODES = 'US'
 
 client = plaid.Client(
   client_id = PLAID_CLIENT_ID,
   secret=PLAID_SECRET,
   public_key=PLAID_PUBLIC_KEY,
   environment=PLAID_ENV,
-  api_version='2019-05-29',
+  api_version='2018-05-22',
   client_app='Plaid Quickstart' # internal flag - don't modify
 )
 
